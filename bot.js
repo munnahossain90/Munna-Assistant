336e689effd9961ad30fca5ff4a7c9642e3ed2bd
munnahossain90 <63153910+munnahossain90@users.noreply.github.com>
2020-04-04 22:21:03 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,30 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'Hi') {
-    	message.reply('Hello');
-  	}
-    if (message.content.startsWith("ping", "hi", "Hello", "hello",)) {
-    message.channel.send("Hi! There...😉");
+client.on('message', msg => {  
+    if(message.author.bot) return;
+    if (msg.content === 'hi') {     
+        msg.reply('Hello!');   
+    }
+    if (msg.content === 'Hi') {     
+        msg.reply('Hello!');   
+    } 
+    if (msg.content === 'Hello') {     
+        msg.reply('Hi!');   
+    }
+    if (msg.content === 'How are you?') {     
+        msg.reply('Great & fully Charged. How about you?');   
+    }
+    if (msg.content === 'Hello') {     
+        msg.reply('Hi!');   
+    }
+    if (msg.content === 'Bot') {     
+        msg.reply('I am a boot  always ready to Help.');   
+    }
+    
+   if (msg.content === 'Welcome') {
+        msg.reply('Good to see you.', {files:     ['https://blog.vantagecircle.com/content/images/size/w730/2019/09/welcome.png']});
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
